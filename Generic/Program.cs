9e0b70d7362bbb5d4758fca347ee937bcd55df67
minal999 <minughadge555@gmail.com>
2022-09-27 19:41:16 +0530

    commit

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -9,7 +9,7 @@ namespace Generic
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }
         public int Price { get; set; }
     }
     internal class Program
@@ -21,11 +21,11 @@ namespace Generic
             list1.Add(20);
             list1.Add(30);
             list1.Add(40);
-            list1.Insert(2,50);
+            list1.Insert(2, 50);
             list1.Remove(2);
             list1.Reverse(1, 4);
             list1.IndexOf(30);
-       
+
             Console.WriteLine($"count is {list1.Count()}");
             Console.WriteLine($"Capacity is {list1.Capacity}");
             //list2.CopyTo(list1,2);
@@ -54,10 +54,7 @@ namespace Generic
             {
                 Console.WriteLine(p);
             }
-
-
-
-
         }
     }
-}
+ }
+    
