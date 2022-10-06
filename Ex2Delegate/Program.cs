57f1090c82ff3dfe6126be97d7b9e0fe631e8049
minal999 <minughadge555@gmail.com>
2022-10-06 19:41:56 +0530

    commit

--- Ex2Delegate/Program.cs
+++ Ex2Delegate/Program.cs
@@ -1,62 +1,68 @@
-﻿using System;
+﻿using Ex2Delegate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ex2Delegate
 {
-    public delegate int myDelegate(int bal);
+    public delegate void myDelegate();
     public class bank
     {
         public event myDelegate LowBalance;
         public event myDelegate InsufficientBalance;
-        public int bal;
+        public int bal { get; set; }
         public int crAmt, dbAmt;
-
-       // public int new_bal;
         public bank(int bal)
         {
             this.bal = bal;
         }
-        public void check(int bal)
+        public void credit(int crAmt)
+        {
+            bal = bal + crAmt;
+            Console.WriteLine($"Balance after credit: {bal}");
+        }
+        public void debit(int dbAmt)
         {
             if (bal < 5000)
             {
                 LowBalance();
-                //Console.WriteLine("Low Balance");
+
             }
             else if (dbAmt > bal)
             {
                 InsufficientBalance();
-                //Console.WriteLine("Insufficient balance");
+                Console.WriteLine($"Your Balance : {bal}");
             }
-            Console.WriteLine($"Your balance is {bal}");
+            else
+            {
+                bal = bal - dbAmt;
+                Console.WriteLine($"Balance after debit: {bal}");
+            }
         }
 
-    }
-    internal class Program
-    {
-        public static  void credit(int bal)
+        internal class Program
         {
-            //Console.WriteLine("Enter Credit amount: ");
-            //int crAmt = Convert.ToInt32(Console.ReadLine());
-            //bal = bal + crAmt;
-            Console.WriteLine($"Balance after credit: {bal}");
-        }
-        public static void debit(int bal)
-        {
-            //Console.WriteLine("Enter debit amount: ");
-            //int dbAmt = Convert.ToInt32((Console.ReadLine()));
-            //bal = bal - dbAmt;
-            Console.WriteLine($"Balance after debit: {bal}");
-        }
-        static void Main(string[] args)
-        {
-            bank b = new bank(25000);
+            public static void lowBalance()
+            {
+                Console.WriteLine("Low Balance");
+            }
+            public static void insufficient()
+            {
+                Console.WriteLine("insufficient balance");
+            }
+            static void Main(string[] args)
+            {
+                bank b = new bank(5005);
 
-            b.LowBalance = new myDelegate(credit);
-
+                b.LowBalance += new myDelegate(lowBalance);
+                b.InsufficientBalance += new myDelegate(insufficient);
+                b.credit(400);
+                b.debit(240000);
+            }
         }
     }
 }
